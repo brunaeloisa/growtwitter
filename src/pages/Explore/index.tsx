61b583785acc5f58c7d65de280427ddad0728181
bruna <brunaeloisa7@gmail.com>
2026-03-31 15:44:59 -0300

    feat: add explore section

--- src/pages/Explore/index.tsx
+++ src/pages/Explore/index.tsx
@@ -1,3 +1,40 @@
+import { Box, Typography } from '@mui/material';
+import { TrendingTopic } from '../../components/TrendingTopic';
+import { topics } from '../../data/trendingTopics';
+
 export function Explore() {
-  return <h1>Explore</h1>;
+  return (
+    <>
+      <Typography
+        component="h1"
+        variant="body2"
+        sx={{
+          p: 1.5,
+          fontWeight: 800,
+          borderBottom: 1,
+          borderColor: 'divider',
+          marginBottom: 1
+        }}
+      >
+        Explorar
+      </Typography>
+
+      <Box
+        component="ul"
+        sx={{
+          listStyle: 'none',
+          p: 0,
+          m: 0
+        }}
+      >
+        {topics.map((topic) => (
+          <TrendingTopic
+            key={topic.id}
+            title={topic.title}
+            category={topic.category}
+          ></TrendingTopic>
+        ))}
+      </Box>
+    </>
+  );
 }
